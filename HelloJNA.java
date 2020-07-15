d402e75bd9bf0c5266e547fc03eb6c71b7f25c7f
ssk <sasat.s.93@gmail.com>
2020-07-15 22:18:57 +0900

    jarを使わなければHelloJNA動作した

--- HelloJNA.java
+++ HelloJNA.java
@@ -1,13 +1,13 @@
-import com.sun.jna.Library;
-import com.sun.jna.Native;
+// import com.sun.jna.Library;
+// import com.sun.jna.Native;
 
-interface HelloLib extends Library {
-  // loadLibrary‚Ì‘æˆêˆø”‚Í‚ ‚Æ‚Åì¬‚·‚élib***.so‚Ì***‚Æˆê’v‚³‚¹‚éB
-  HelloLib INSTANCE = (HelloLib) Native.loadLibrary("hello", HelloLib.class);
+// interface HelloLib extends Library {
+//   // loadLibrary‚Ì‘æˆêˆø”‚Í‚ ‚Æ‚Åì¬‚·‚élib***.so‚Ì***‚Æˆê’v‚³‚¹‚éB
+//   HelloLib INSTANCE = (HelloLib) Native.loadLibrary("hello", HelloLib.class);
 
-  // C‚ÌŠÖ”–¼‚Æˆê’v‚³‚¹‚é
-  void hello();
-}
+//   // C‚ÌŠÖ”–¼‚Æˆê’v‚³‚¹‚é
+//   void hello();
+// }
 
 public class HelloJNA {
   public static void main(String[] args){
--- make_hello_java.bat
+++ make_hello_java.bat
@@ -1,2 +1,2 @@
-javac -cp jna-4.1.0.jar HelloJNA.java
-java -cp .:jna-4.1.0.jar HelloJNA
+javac HelloJNA.java
+java HelloJNA

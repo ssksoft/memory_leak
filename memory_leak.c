e8fa1e4ebe17578648946e15093d1b8f10f1769d
ssk <sasat.s.93@gmail.com>
2020-07-15 20:43:11 +0900

    メモリリーク成功。 PCが動作しなくなった

---
+++ memory_leak.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <memory.h>
+
+int main(int argc, char **args)
+{
+    int i;
+    char *p;
+    while (1)
+    {
+        //  ƒƒ‚ƒŠ‚ğ¶¬iŠJ•ú‚µ‚È‚¢j
+        p = (char *)malloc(sizeof(char) * 10000);
+    }
+}
